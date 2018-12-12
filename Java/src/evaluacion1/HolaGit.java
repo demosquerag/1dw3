90607e05121a4a8287f538353e2f77e966f57c65
D3M6 <demg.working@gmail.com>
2018-12-12 13:29:08 +0100

    Version 0.11

--- Java/src/evaluacion1/HolaGit.java
+++ Java/src/evaluacion1/HolaGit.java
@@ -5,7 +5,7 @@ public class HolaGit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Hola git");
+		System.out.println("Hola git, 2");
 	}
 
 }
