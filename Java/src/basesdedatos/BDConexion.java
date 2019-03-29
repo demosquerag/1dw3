32a27417f769cb219e78eea4da34851bced01a1b
D3M6 <demg.working@gmail.com>
2019-03-29 14:15:47 +0100

    Version 1.1.1

--- Java/src/basesdedatos/BDConexion.java
+++ Java/src/basesdedatos/BDConexion.java
@@ -2,6 +2,7 @@ package basesdedatos;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -20,6 +21,19 @@ public class BDConexion {
 			
 			// Creo un statement
 			Statement st = conexion.createStatement();
+			
+			// Preparo una consulta
+			String consulta = "SELECT * FROM alumnos";
+			// Ejecuto la consulta
+			ResultSet rs = st.executeQuery("SELECT * FROM alumnos");
+			// RECORRO LOS REGISTROS DEVUELTOS
+			while (rs.next()){
+				System.out.println("DNI: " + rs.getObject("dni") + ", nombre: " + rs.getObject("nombre") + ", Apellidos: " + rs.getObject("apellidos") + ", Grupo: " + rs.getObject("grupo"));
+			}
+			// cierro el ResultSet
+			rs.close();
+			// cierro el Statement despues de realizar la consulta
+			st.close();
 			
 			// Cierro conexion
 			conexion.close();
