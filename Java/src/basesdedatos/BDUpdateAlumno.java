fc2387f221e86e0b265cad604208156e100373c5
D3M6 <demg.working@gmail.com>
2019-04-01 12:30:51 +0200

    Version 1.1.4

---
+++ Java/src/basesdedatos/BDUpdateAlumno.java
@@ -0,0 +1,46 @@
+package basesdedatos;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class BDUpdateAlumno {
+
+	public static void main(String[] args) {
+		
+		try {
+			
+			// Conectar a la base de datos
+			Connection conexion = DriverManager.getConnection("jdbc:mysql://localhost/bdalumnos", "root", "");
+			
+			// Salida por consola
+			System.out.println("Conexion correcta.");
+			
+			// Creo un statement
+			Statement st = conexion.createStatement();
+			
+			// Preparo una consulta
+			String consulta = "SELECT * FROM alumnos";
+			// Ejecuto la consulta
+			ResultSet rs = st.executeQuery(consulta);
+			// RECORRO LOS REGISTROS DEVUELTOS
+			while (rs.next()){
+				System.out.println("DNI: " + rs.getObject("dni") + ", nombre: " + rs.getObject("nombre") + ", Apellidos: " + rs.getObject("apellidos") + ", Grupo: " + rs.getObject("grupo"));
+			}
+			// cierro el ResultSet
+			rs.close();
+			// cierro el Statement despues de realizar la consulta
+			st.close();
+			
+			// Cierro conexion
+			conexion.close();
+			
+		} catch (SQLException sqle) {
+			sqle.printStackTrace();
+		}
+
+	}
+
+}
