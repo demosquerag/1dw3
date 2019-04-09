17a73dd2420b4ea79d49de4777b70d0430cb83fc
D3M6 <demg.working@gmail.com>
2019-04-09 22:17:52 +0200

    Version 1.1.10

--- Java/src/basesdedatos/BDConexion2.java
+++ Java/src/basesdedatos/BDConexion2.java
@@ -6,7 +6,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-
 public class BDConexion2 {
 
 	public static void main(String[] args) {
@@ -15,26 +14,25 @@ public class BDConexion2 {
 			
 			// Conectar a la base de datos
 			Connection conexion = DriverManager.getConnection("jdbc:mysql://localhost/bizkaiabasket", "root", "");
-			
 			// Salida por consola
 			System.out.println("Conexion correcta.");
-			
 			// Creo un statement
 			Statement st = conexion.createStatement();
-						
 			// Preparo una consulta
-			String consulta = "SELECT * FROM usuarios";
+			String consulta = "SELECT usuario,tipo FROM usuarios";
 			// Ejecuto la consulta
 			ResultSet rs = st.executeQuery(consulta);
 			// RECORRO LOS REGISTROS DEVUELTOS
 			while (rs.next()){
-				System.out.println("Nick: " + rs.getObject("nickname") + ", Nombre: " + rs.getObject("nombre") + ", Apellidos: " + rs.getObject("apellidos") + ", Tipo: " + rs.getObject("tipo"));
+				System.out.println("Usuario: " + rs.getObject("usuario") + ", Tipo: " + rs.getObject("tipo") + ".");
+				if (rs.getObject("usuario").equals("Daniel") && rs.getObject("tipo").equals("U")) {
+					System.out.println("Perfes");
+				}
 			}
 			// cierro el ResultSet
 			rs.close();
 			// cierro el Statement despues de realizar la consulta
 			st.close();
-			
 			// Cierro conexion
 			conexion.close();
 			
