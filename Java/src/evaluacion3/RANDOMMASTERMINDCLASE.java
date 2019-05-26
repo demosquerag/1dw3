77fa39a801d7644d4e48ee2d9d986a0d74f31d56
D3M6 <demg.working@gmail.com>
2019-05-26 23:00:35 +0200

    Version 1.1.26

--- Java/src/evaluacion3/RANDOMMASTERMINDCLASE.java
+++ Java/src/evaluacion3/RANDOMMASTERMINDCLASE.java
@@ -8,64 +8,67 @@ public class RANDOMMASTERMINDCLASE {
 
 	public static void main(String[] args) {
 		
-		//genero un numero de 4 cifras diferentes
+		// Genero un numero de 4 cifras diferentes
 		ArrayList<Integer> numerocorrecto = new ArrayList<Integer>();
-			
-			Random rnd = new Random();
-			int cifra;
-			String numero = "";
-			int posicioncorrecta = 0;
-			int cifrascorrectas = 0;
-			int posicionescorrectas = 0;
-			do {
-			//genero una cifra
+		Random rnd = new Random();
+		String numero = "";
+		// Defino un objeto de la clase escaner
+		Scanner teclado;
+		int cifra;
+		int posicioncorrecta = 0;
+		int cifrascorrectas = 0;
+		int posicionescorrectas = 0;
+		
+		do {
+			// Genero una cifra
 			cifra = ((int)(rnd.nextDouble()*100000.0))%10;
-			//compruebo si esta en el arraylist
-			if(!numerocorrecto.contains(cifra)) {
-				//si no esta en el arraylist
-				//la añado
+			// Compruebo si esta en el arraylist
+			if (!numerocorrecto.contains(cifra)) {
+				// Si no esta en el arraylist la agrego
 				numerocorrecto.add(cifra);
-			 }
 			}
-			while (numerocorrecto.size()<4);
 			
-			//muestro el numero generado
-			System.out.println("Numero generado: " + numerocorrecto);
-			
-			//defino un objeto de la clase escaner
-			Scanner teclado;
-			//creo un objeto de la clase escaner
-			teclado = new Scanner(System.in);
-			
-			//pido numeros de cuatro cifras
-			//hasta wue se acierte el numero
-			
-			do {
+		} while (numerocorrecto.size()<4);
+		
+		// Muestro el numero generado
+		System.out.println("Numero generado: " + numerocorrecto);
+		
+		// Creo un objeto de la clase escaner
+		teclado = new Scanner(System.in);
+		
+		// Pido numeros de cuatro cifras hasta que se acierte el numero
+		do {
+			// Pedir numero por teclado
 			System.out.println("Introduce un numero:" );
 			numero = teclado.nextLine();
-			if(numero.length()==4) {
-				//si el numero tiene cuatro cifras
-				//compruebo si hay alguna cifra en el numero correcto
-				//por cada cifra indico si coincide la posicion
+			if (numero.length() == 4) {
+				// Si el numero tiene cuatro cifras
+				// Compruebo si hay alguna cifra en el numero correcto
+				// Por cada cifra indico si coincide la posicion
 				for(int posicion = 0; posicion<4; posicion++) {
-				cifra = numero.charAt(posicion)-'0';
-				System.out.println("Cifra: " + cifra);
-				//compruebo si hay alguna cifra en el numero correcto
-				posicioncorrecta = numerocorrecto.indexOf(cifra); // devuelve la posocion o -1 no la encuentra
+					cifra = numero.charAt(posicion)-'0';
+					System.out.println("Cifra: " + cifra);
+					// Compruebo si hay alguna cifra en el numero correcto
+					posicioncorrecta = numerocorrecto.indexOf(cifra); // devuelve la posocion o -1 no la encuentra
+					
 					if(posicioncorrecta != -1) {
-						// compruebo si coincide la posicion
+						// Compruebo si coincide la posicion
 						cifrascorrectas = cifrascorrectas+ 1;
 						if (posicion == posicioncorrecta) {
-							//si la posicion esta en la misma posicion
+							// Si la posicion esta en la misma posicion
 							posicionescorrectas = posicionescorrectas+ 1;
 						}
 					}
+					
 				}
-				
-				System.out.println("Cifras correctas: "+ cifrascorrectas + ", Cifras en la posicion correcta: " + posicionescorrectas);
-			}
-			} while (posicionescorrectas < 4);
 			
+				// Salida de texto del numero acertado
+				System.out.println("Cifras correctas: "+ cifrascorrectas + ", Cifras en la posicion correcta: " + posicionescorrectas);
+			
+			}
+		
+		} while (posicionescorrectas < 4);
+		
 		//cierro teclado
 		teclado.close();
 	}
