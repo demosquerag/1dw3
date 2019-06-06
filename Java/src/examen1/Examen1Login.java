0c602b65d6416a68aee65573746fab8e8b44a317
D3M6 <demg.working@gmail.com>
2019-06-06 16:32:32 +0200

    Version 1.1.51

--- Java/src/examen1/Examen1Login.java
+++ Java/src/examen1/Examen1Login.java
@@ -6,12 +6,31 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.text.JTextComponent;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JPasswordField;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
 
-public class Examen1Login extends JFrame {
+import javax.swing.SwingConstants;
+
+public class Examen1Login extends JFrame implements ActionListener, FocusListener{
 
 	// Necesidades pervias
 	private static final long serialVersionUID = 4923947275106098399L;
-	private JPanel contentPane;
+	private JPanel contenedor;
+	private JPanel panelLogin;
+	private JLabel lblUsuario;
+	private JLabel lblPassword;
+	private JLabel lblMensaje;
+	private JTextField txtUsuario;
+	private JPasswordField passwordField;
+	
+	private int intentos = 3;
 
 	
 	public static void main(String[] args) {
@@ -29,12 +48,81 @@ public class Examen1Login extends JFrame {
 
 	// Metodo de la interfaz grafica
 	public Examen1Login() {
+		setTitle("Examen de Daniel Mosquera");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
-		setContentPane(contentPane);
+		setBounds(100, 100, 651, 114);
+		contenedor = new JPanel();
+		contenedor.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contenedor.setLayout(new BorderLayout(0, 0));
+		setContentPane(contenedor);
+		
+		panelLogin = new JPanel();
+		contenedor.add(panelLogin, BorderLayout.NORTH);
+		
+		lblUsuario = new JLabel("Usuario");
+		lblUsuario.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		panelLogin.add(lblUsuario);
+		
+		txtUsuario = new JTextField();
+		txtUsuario.addActionListener(this);
+		txtUsuario.addFocusListener(this);
+		panelLogin.add(txtUsuario);
+		txtUsuario.setColumns(10);
+		
+		lblPassword = new JLabel("Password");
+		lblPassword.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		panelLogin.add(lblPassword);
+		
+		passwordField = new JPasswordField();
+		passwordField.addActionListener(this);
+		passwordField.addFocusListener(this);
+		passwordField.setText("*****");
+		panelLogin.add(passwordField);
+		
+		lblMensaje = new JLabel("Listo");
+		lblMensaje.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		lblMensaje.setHorizontalAlignment(SwingConstants.CENTER);
+		contenedor.add(lblMensaje, BorderLayout.SOUTH);
+	}
+	
+	private void login() {
+		// Necesidades previas
+		String user = "daniel";
+		String password = "mosquera";
+		String txtPassword = new String (passwordField.getPassword());
+		
+		if (txtUsuario.getText().equals(user) && txtPassword.equals(password)) {
+			Examen1Menu frame = new Examen1Menu();
+			frame.setVisible(true);
+			frame.setTitle("Bienvenido: "+user);
+			dispose();
+		} else {
+			intentos --;
+			lblMensaje.setText("Error.Los datos no son correctos. Le quedan "+intentos+".");
+		}
+		
+		if (intentos == 0) {
+			System.exit(0);
+		}
+	}
+
+	@Override
+	public void focusGained(FocusEvent fe) {
+		JTextComponent jtc = (JTextComponent) fe.getSource();
+		jtc.setSelectionStart(0);
+		jtc.setSelectionEnd(jtc.getText().length());
+	}
+
+	@Override
+	public void focusLost(FocusEvent fe) {
+		JTextComponent jtc = (JTextComponent) fe.getSource();
+		jtc.setSelectionStart(0);
+		jtc.setSelectionEnd(0);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent ap) {
+		login();
 	}
 
 }
--- Java/src/examen1/Examen1Menu.java
+++ Java/src/examen1/Examen1Menu.java
@@ -5,21 +5,146 @@ import java.awt.BorderLayout;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import java.awt.GridLayout;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Examen1Menu extends JFrame {
 
 	// Necesidades previas
 	private static final long serialVersionUID = 5215068799520615230L;
-	private JPanel contentPane;
+	private JPanel contenedor;
+	private JPanel panelBotones;
+	private JLabel lblResultado;
+	private JTextField txtTexto;
+	private JButton btnPrimo;
+	private JButton btnCircunferencia;
+	private JButton btnMultiplos;
+	private JButton btnParimpar;
 
 	// Metodo de la interfaz grafica
 	public Examen1Menu() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
-		setContentPane(contentPane);
+		contenedor = new JPanel();
+		contenedor.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contenedor.setLayout(new BorderLayout(0, 0));
+		setContentPane(contenedor);
+		
+		txtTexto = new JTextField();
+		txtTexto.setFont(new Font("Tahoma", Font.BOLD, 11));
+		txtTexto.setHorizontalAlignment(SwingConstants.RIGHT);
+		contenedor.add(txtTexto, BorderLayout.NORTH);
+		txtTexto.setColumns(10);
+		
+		lblResultado = new JLabel("Resultado");
+		lblResultado.setFont(new Font("Tahoma", Font.BOLD, 12));
+		lblResultado.setHorizontalAlignment(SwingConstants.CENTER);
+		contenedor.add(lblResultado, BorderLayout.SOUTH);
+		
+		panelBotones = new JPanel();
+		contenedor.add(panelBotones, BorderLayout.CENTER);
+		panelBotones.setLayout(new GridLayout(0, 2, 0, 0));
+		
+		btnPrimo = new JButton("EsPrimo");
+		btnPrimo.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				btnPrimo();
+			}
+		});
+		panelBotones.add(btnPrimo);
+		
+		btnCircunferencia = new JButton("Circunferencia");
+		btnCircunferencia.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				btnCircunferencia();
+			}
+		});
+		panelBotones.add(btnCircunferencia);
+		
+		btnMultiplos = new JButton("10 Multiplos");
+		btnMultiplos.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				btnMultiplos();
+			}
+		});
+		panelBotones.add(btnMultiplos);
+		
+		btnParimpar = new JButton("Par o Impar");
+		btnParimpar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				btnParimpar();
+			}
+		});
+		panelBotones.add(btnParimpar);
+	}
+	
+	// Metodo para saber si un numeor es primo o no
+	private void btnPrimo() {
+		// Necesidades previas
+		int num = Integer.parseInt(txtTexto.getText());
+		int num2 = 0;
+		boolean primo = false;
+		
+		// Resto al numero divisible un numero
+		num2 = num - 1;
+		while ((num % num2) != 0) {
+			num2--;
+		}
+		
+		// Si el calculo es correcto...
+		if (num2 == 1) {
+			primo = true;
+		}
+		// Si es primo
+		if (primo == true) {
+			lblResultado.setText("El numero "+num2+" es primo.");
+		} else { // Si no...
+			lblResultado.setText("El numero "+num2+" no es primo.");
+		}
+		
+	}
+	
+	// Metodo para hacer el calculo del area de la circunferencia
+	private void btnCircunferencia() {
+		// Necesidades previas
+		double radio = Double.parseDouble(txtTexto.getText());
+		double resultado = 0.0;
+		
+		// Calculo el valor del area
+		resultado = Math.PI * Math.pow(radio,2);
+		// imprimo el resultado en la label
+		lblResultado.setText("El valor de la circunferencia de radio "+radio+ " es "+resultado+".");
+	}
+
+	// Metodo para sacar los diez primeros multiplos de un numero
+	private void btnMultiplos() {
+		// Necesidades previas
+		double multiplo = Double.parseDouble(txtTexto.getText());
+		
+		// imprimo el resultado en la label
+		lblResultado.setText("Los 10 primeros multiplos de "+multiplo+ " son "+multiplo*1+" "+multiplo*2
+				+" "+multiplo*3+" "+multiplo*4+" "+multiplo*5+" "+multiplo*6+" "+multiplo * 7+" "+multiplo*8
+				+" "+multiplo*9+" "+multiplo*10+".");
+	}
+
+	// Metodo para el calculo de numeros pares e impares
+	private void btnParimpar() {
+		// Necesidades previas
+		int num = Integer.parseInt(txtTexto.getText());
+		
+		// Hacemos el calculo
+		if (num %2 == 0) {
+			// Sacamos resultado
+			lblResultado.setText("El "+num+" es par.");
+		} else {
+			lblResultado.setText("El "+num+" es impar.");
+		}
 	}
 
 }
